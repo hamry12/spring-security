440257fb1cbc73d7a29b9cd430d340956087062e
Himanshu <h_himanshu2007@yahoo.com>
2024-12-03 21:32:02 +0530

    update: Authentication Provider

--- src/main/java/com/example/spring_security/config/ApplicationSecurity.java
+++ src/main/java/com/example/spring_security/config/ApplicationSecurity.java
@@ -2,36 +2,36 @@ package com.example.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ApplicationSecurity {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests((requests) -> {
-            requests
-                    .requestMatchers("/myAccount", "/myLoan", "/myBalance")
-                    .authenticated();
+        http.
+                requiresChannel(
+                        (requireChannelConfig)->requireChannelConfig
+                                .anyRequest()
+                                .requiresInsecure()
+                ).
+                csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests((requests) -> {
+                    requests
+                            .requestMatchers("/myAccount", "/myLoan", "/myBalance")
+                            .authenticated();
 
-            requests
-                    .requestMatchers("/","/notices", "/registerUser")
-                    .permitAll();
-        });
+                    requests
+                            .requestMatchers("/","/notices", "/registerUser")
+                            .permitAll();
+                });
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
--- src/main/java/com/example/spring_security/controller/AccountController.java
+++ src/main/java/com/example/spring_security/controller/AccountController.java
@@ -2,6 +2,8 @@ package com.example.spring_security.controller;
 
 import com.example.spring_security.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,8 +14,8 @@ public class AccountController {
     private CustomerRepository customerRepository;
 
     @GetMapping("/myAccount")
-    public String getAccountDetails(){
-        return "Welcome to Spring Security!!!";
+    public ResponseEntity<String> getAccountDetails(){
+        return ResponseEntity.status(HttpStatus.OK).body("Welcome to Spring Security!!!");
 
     }
 }
